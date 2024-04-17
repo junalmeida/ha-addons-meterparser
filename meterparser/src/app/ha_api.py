ab5c1fde24889efa4422da1bf80eb9ffee118ac4
Marcos Junior <marcos.almeida@powerschool.com>
2024-04-17 08:33:27 -0500

    Update base image; Add checks for env variables

--- meterparser/src/app/ha_api.py
+++ meterparser/src/app/ha_api.py
@@ -9,6 +9,10 @@ supervisor_api = "http://supervisor/%s"
 
 
 def supervisor(param: str):
+    if not "SUPERVISOR_TOKEN" in os.environ:
+        _LOGGER.error("SUPERVISOR_TOKEN not set.")
+        sys.exit(1)
+
     while True:
         try:
             _LOGGER.debug("Connecting to supervisor...")    
@@ -25,35 +29,38 @@ def supervisor(param: str):
             _LOGGER.error("Could not connect to supervisor: %s. Retry in 5 secs." % e)
             time.sleep(5)
 def version() -> str:
-    try:
-        if "SUPERVISOR_TOKEN" in os.environ:
-            supervisor_auth = {
-                "Authorization": "Bearer %s" % os.environ['SUPERVISOR_TOKEN']
-            }
+    if not "SUPERVISOR_TOKEN" in os.environ:
+        _LOGGER.error("SUPERVISOR_TOKEN not set.")
+        sys.exit(1)
 
-            result = requests.get(supervisor_api % ("addons/self/info"), headers=supervisor_auth)
-            result_json = result.json()
-            result_json = result_json["data"] if "data" in result_json else result_json
-            return result_json["version"] if "version" in result_json else "0.0.0.0"
-        else:
-            return "0.0.0.0"
+    try:
+        supervisor_auth = {
+            "Authorization": "Bearer %s" % os.environ['SUPERVISOR_TOKEN']
+        }
+
+        result = requests.get(supervisor_api % ("addons/self/info"), headers=supervisor_auth)
+        result_json = result.json()
+        result_json = result_json["data"] if "data" in result_json else result_json
+        return result_json["version"] if "version" in result_json else "0.0.0.0"
     except Exception as e:
         _LOGGER.error("Could not connect to supervisor: %s" % e)
         return "0.0.0.0"
 
 def hostname() -> str:
-    try:
-        if "SUPERVISOR_TOKEN" in os.environ:
-            supervisor_auth = {
-                "Authorization": "Bearer %s" % os.environ['SUPERVISOR_TOKEN']
-            }
+    if not "SUPERVISOR_TOKEN" in os.environ:
+        _LOGGER.error("SUPERVISOR_TOKEN not set.")
+        sys.exit(1)
 
-            result = requests.get(supervisor_api % ("addons/self/info"), headers=supervisor_auth)
-            result_json = result.json()
-            result_json = result_json["data"] if "data" in result_json else result_json
-            return result_json["hostname"] if "hostname" in result_json else "unknown"
-        else:
-            return "unknown"
+    try:
+        supervisor_auth = {
+            "Authorization": "Bearer %s" % os.environ['SUPERVISOR_TOKEN']
+        }
+
+        result = requests.get(supervisor_api % ("addons/self/info"), headers=supervisor_auth)
+        result_json = result.json()
+        result_json = result_json["data"] if "data" in result_json else result_json
+        return result_json["hostname"] if "hostname" in result_json else "unknown"
+
     except Exception as e:
         _LOGGER.error("Could not connect to supervisor: %s" % e)
         return "unknown"        
