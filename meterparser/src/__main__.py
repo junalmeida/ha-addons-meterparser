cb18a909f2bff18260439057fe99976a081c2a17
Marcos Junior <marcos.almeida@powerschool.com>
2024-04-17 14:07:17 -0500

    Better log format

--- meterparser/src/__main__.py
+++ meterparser/src/__main__.py
@@ -1,19 +1,26 @@
+import logging
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--token', required=True)
+parser.add_argument('--debug', required=False)
+known_args, unknown_args = parser.parse_known_args()
+token = known_args.token
+
+logging.basicConfig(level=logging.DEBUG if known_args.debug else logging.INFO, format='[%(asctime)s] %(levelname)s: [%(name)s] %(message)s', datefmt='%H:%M:%S')
+_LOGGER = logging.getLogger(__name__)
+
 from app.mqtt import Mqtt
 from app.service import Service
 import sys
 import signal
-import argparse
-import logging
-_LOGGER = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--token', required=True)
-known_args, unknown_args = parser.parse_known_args()
-token = known_args.token
+
 
 if not token or token == "":
     _LOGGER.error("No token provided. Exiting...")
     sys.exit(1)
+_LOGGER.info("Meterparser started")
 
 mqtt = Mqtt(token)
 service = Service(mqtt.cameras)
@@ -27,4 +34,5 @@ def run():
     mqtt.mqtt_start() # locks the execution
 
 signal.signal(signal.SIGINT, signal_handler)
+
 run()
